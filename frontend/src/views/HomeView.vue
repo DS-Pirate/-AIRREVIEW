e60940dc769c810d4b129ccd0b5ee0996d069985
Hyunseok Byun <gumyoincirno@gmail.com>
2022-08-07 15:08:33 +0900

    add anchor tag on maincard for route to /read

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,15 +3,14 @@
     <h2>최근 올라온 글</h2>
 		<div class="fav rounded-3 p-3 m-0 shadow-sm w-100 bg-white">
 			<div class="row row-cols-3 gx-4 gy-3">
-				<Cards v-for="i in 32" :key="i" class="col"> Column </Cards>
+				<!-- this will be changed to using store.obj (Vuex statement)  -->
+				<Cards :cardinfo="i" v-for="i in 32" :key="i" class="col"> Column </Cards>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 import Cards from "@/components/MainCards.vue";
 
 export default {
