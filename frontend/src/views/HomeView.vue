df2a77bf35ab48097a5e9a8674e50d3d4c6949c3
Kim-DH0 <101510236+Kim-DH0@users.noreply.github.com>
2022-08-08 09:05:01 +0900

    Revert "Merge remote-tracking branch 'origin/SECOND-STAGE' into DOHYEON"
    This reverts commit ea34e644ba01eb6b7c180d5cf22108a53a6ed1b1, reversing
    changes made to d6592a5d5528a40d8c626f4b83f09fde2735d21d.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,22 @@
 <template>
-	<div class="mainviewpage">
-    <h2>최근 올라온 글</h2>
-		<div class="fav rounded-3 p-3 m-0 shadow-sm w-100 bg-white">
-			<div class="row row-cols-3 gx-4 gy-3">
-				<!-- this will be changed to using store.obj (Vuex statement)  -->
-				<Cards :cardinfo="i" v-for="i in 32" :key="i" class="col"> Column </Cards>
-			</div>
-		</div>
-	</div>
+  <div class="mainviewpage">
+
+
+  <div class="row row-cols-3">
+
+    <Cards v-for="i in 32" :key="i" class="col">
+      Column
+    </Cards>
+
+  </div>
+
+  </div>
 </template>
 
 <script>
-import Cards from "@/components/MainCards.vue";
+// @ is an alias to /src
+
+import Cards from '@/components/MainCards.vue'
 
 export default {
   name: 'HomeView',
--- frontend/src/views/MychannelPage.vue
+++ frontend/src/views/MychannelPage.vue
@@ -1,43 +1,20 @@
 <template>
 	<channelpage>
-		<div class="chimg rounded-2 p-3 m-0 shadow-sm w-100 bg-white">
-			<img src="@/assets/longdummy2.jpeg" class="ch_img w-100" alt="" />
-		</div>
-		<div class="chcategory rounded-2 p-1 my-3 shadow-sm bg-white d-flex justify-content-center">
-			<ul class="list-group list-group-horizontal h-10 align-items-center">
-				<li class="list-group-item border border-0" v-on:click="change('list')">글 목록</li>
-				<span>|</span>
-				<li class="list-group-item border border-0" v-on:click="change('category')">구독 정보</li>
-				<span>|</span>
-				<li class="list-group-item border border-0" v-on:click="change('channelinfo')">내 정보</li>
-			</ul>
-		</div>
-		<div class="chcategory rounded-2 p-3 my-3 shadow-sm bg-white d-flex h-50" v-if="(state.form == 'list')">
-			<span>list</span>
-		</div>
-		<div class="chcategory rounded-2 p-3 my-3 shadow-sm bg-white d-flex h-50" v-else-if="(state.form == 'category')">
-			<span>cate</span>
-		</div>
-		<div class="chcategory rounded-2 p-3 my-3 shadow-sm bg-white d-flex h-50" v-else-if="(state.form == 'channelinfo')">
-			<span>info</span>
+		<div class="ch rounded-3 p-3 m-0 shadow-sm w-100 bg-white">
+            <img src="@/assets/longdummy2.jpeg" class="ch_img w-100" alt="">
+				
+			
 		</div>
 	</channelpage>
 </template>
 <script>
-import { reactive } from "vue";
+// @ is an alias to /src
+
 
 export default {
-	components: { },
+	name: "MyChannel",
+	components: {
 
-	setup() {
-		let state = reactive({
-			form: "list",
-		});
-		function change(which) {
-			state.form = which;
-			console.log(state.form);
-		}
-		return { state, change };
 	},
 };
 </script>
