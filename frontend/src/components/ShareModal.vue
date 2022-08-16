2f162577fb8dae56ce66725aa2e5ced0b4b23f35
Kim-DH0 <101510236+Kim-DH0@users.noreply.github.com>
2022-08-16 11:55:53 +0900

    08-16 공유 폼 추가

---
+++ frontend/src/components/ShareModal.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="modal" id="share" tabindex="-1" aria-labelledby="share" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content  rounded-0">
+        <div class="modal-header">
+          <h5 class="modal-title" id="share">글 퍼가기</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+
+           
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="modal-btn border-0 m-0">복사</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+  .modal-btn{
+  position: relative;
+  background-color: white;
+  color: black;
+  padding: 0.4em 1.2em;
+  cursor:pointer;
+  user-select:none;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  transition-duration: 0.4s;
+  -webkit-transition-duration: 0.4s; /* Safari */
+}
+
+.modal-btn:hover {
+  transition-duration: 0.1s;
+}
+
+.modal-btn:after {
+  content: "";
+  display: block;
+  position: absolute;
+  left: 0;
+  top:0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  transition: all 0.5s;
+  box-shadow: 0 0 10px 40px white;
+}
+
+.modal-btn:active:after {
+  box-shadow: 0 0 0 0 white;
+  position: absolute;
+  left: 0;
+  top:0;
+  opacity: 1;
+  transition: 0s;
+}
+
+.modal-btn:active {
+  top: 1px;
+  background-color: #ebe8e8;
+}
+</style>
