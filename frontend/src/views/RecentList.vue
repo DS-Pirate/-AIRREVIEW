a1e2316bcc650f6195b16b2358c71346d599e023
Hyunseok Byun <gumyoincirno@gmail.com>
2022-08-17 17:50:32 +0900

    add recent list and routing

---
+++ frontend/src/views/RecentList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="searchlist">
+  <div class="">
+    <!-- 검색필터 -->
+    <div class="btn-group" role="group" aria-label="Default button group">
+      <h2>최근에 조회한 글</h2>
+    </div>
+
+    <!-- 검색카드 -->
+    <div class="row row-cols-1 my-5">
+      <Cards v-for="i in 32" :key="i" class="col"> Column </Cards>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Cards from "@/components/MainCards.vue";
+
+export default {
+  name: "SearchList",
+  components: {
+    Cards,
+  },
+};
+</script>
+
+<style></style>
