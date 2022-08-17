34bbc695d9cd18cbb8fe47d0274399ff9d6dc592
sjiyeong12 <105151230+sjiyeong12@users.noreply.github.com>
2022-08-17 12:08:48 +0900

    +(22.08.07)Add subscriptionInfo

---
+++ frontend/src/components/MyInfo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="myInfo">
+        <div class="d-flex">
+            <div class="p-2 w-100">Flex item</div>
+            <div class="p-2 flex-shrink-1">Flex item</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyInfo',
+    data() {
+        return {
+
+        }
+    }
+};
+</script>
