a1e703fa9fc8b2c4dc637797b3701d9784b00782
sjiyeong12 <105151230+sjiyeong12@users.noreply.github.com>
2022-09-02 17:50:09 +0900

    [JIYOUNG] +(22.09.02.18:00

--- frontend/src/components/SiteLogin.vue
+++ frontend/src/components/SiteLogin.vue
@@ -11,34 +11,65 @@
         <div class="px-5 my-4">
             <button @click="submit()" class="btn btn-outline-primary w-100">로그인</button>
         </div>
-        
-    <!-- </form> -->
+
     </div>
 
 </template>
-<script>
-import { reactive } from "vue";
+<script charset='utf-8'>
+import {reactive} from '@vue/reactivity';
 import axios from 'axios';
+import { useRouter } from 'vue-router';
 
 export default {
-    setup(){
-        const state = reactive({
-            form:{
-                email:"",
-                password:""
-            }
-        })
+  setup(){
+    const router  = useRouter();
+    const state = reactive({
+      form:{
+        email:"",
+        password:""
+      }
+    })
+    const submit =async()=>{
+      if(state.email === ''){
+        alert('아이디를 확인해주세요');
+        state.form.email.value.focus();
+        return false;
+      } else if(state.pw === ''){
+        alert('비밀번호를 확인해주세요');
+        state.form.password.value.focus();
+        return false;
+      }
 
-    const submit =()=>{
-        axios.post("/airreview/api/account/login", state.form).then((res)=>{
-        console.log(res);
-        window.alert("로그인에 성공하셨습니다.");
-        })
+      console.log(`id : `+state.form.email+', passwd : '+state.form.password);
+      //아직 하는 중!!!!
+      const url     = "/airreview/api/"
+      const headers = { "Content-Type": "application/json; charset=utf-8"}
+      const body    = { e_mail: state.form.email, passwd: state.form.password }
+      await axios.post(url, body, {headers}).then(function(res){
+        if(res.data != null){
+          sessionStorage.setItem("TOKEN", res.data.token)
+          sessionStorage.setItem("e_mail", res.data.email)
+          alert('로그인되었습니다.')
+          console.log(res);
+          router.push({name: "Login"})
+        } else {
+          console.log(res);
+          alert('로그인 실패하였습니다.')
+        }
+      })
+
+
+
+      // axios.post("/airreview/api/account/login", state.form).then((res)=>{
+      // console.log(res);
+      // window.alert("로그인에 성공하셨습니다.");
+      // })
     }
-        return{state, submit}
-    }
+    return{state, submit}
+  }
 }
 </script>
+
 <style lang="">
     
 </style>
