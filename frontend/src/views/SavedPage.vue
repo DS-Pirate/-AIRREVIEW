55381498c9fd35b69856310b6b6a87eebd8cdf4f
Kim-DH0 <101510236+Kim-DH0@users.noreply.github.com>
2022-09-19 17:48:32 +0900

    [도현] 2022-09-19
    저장글

--- frontend/src/views/SavedPage.vue
+++ frontend/src/views/SavedPage.vue
@@ -3,7 +3,9 @@
     <h2>저장된 글</h2>
 		<div class="fav p-4 m-0 w-100 bg-white">
 			<div class="row row-cols-2 gx-5 gy-3">
-				<Cards v-for="i in 32" :key="i" class="col"> Column </Cards>
+				<div v-for="(card, idx) in state.cards" :key="idx">
+          <Cards :card="card"></Cards>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -13,11 +15,34 @@
 // @ is an alias to /src
 
 import Cards from "@/components/SaveCards.vue";
+import axios from "axios";
+import { useStore } from 'vuex'
+import {reactive} from "@vue/reactivity";
+
 
 export default {
 	name: "SavedPage",
-	components: {
-		Cards,
-	},
+	components: {Cards},
+	setup() {
+    const store = useStore();
+		const state = reactive({
+      cards: []
+    })
+
+		const url = "/airreview/api/save";
+		const headers = {
+            "Content-Type": "application/json; charset=utf-8",
+            "Authorization": store.state.token,
+            "userid" : store.state.userid
+    }
+		let body = {userid : store.state.userid}
+
+    axios.post(url, body, { headers })
+			.then((res) => {
+				state.cards = res.data;
+				console.log(res);
+    })
+    return {state}
+	}
 };
 </script>
