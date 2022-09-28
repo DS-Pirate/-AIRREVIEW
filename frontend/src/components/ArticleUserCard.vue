af0ff5f8479abbe613a5575d0ddc05789b6f94d2
Hyunseok Byun <gumyoincirno@gmail.com>
2022-09-28 17:27:44 +0900

    Add link for article author

--- frontend/src/components/ArticleUserCard.vue
+++ frontend/src/components/ArticleUserCard.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="channel">
-        <div class="metadata">
-            <img ref="IuserIMG" src="@/assets/pngwing.com.png" alt="" />
-            <div class="info">
-                <span class="name" ref="IairName"></span>
-                <span class="subscribers" ref="IsubCount"></span>
+        <a :href="'./mypage?channel=' + store.state.authorid">
+            <div class="metadata">
+                <img ref="IuserIMG" src="@/assets/pngwing.com.png" alt="" />
+                <div class="info">
+                    <span class="name" ref="IairName"></span>
+                    <span class="subscribers" ref="IsubCount"></span>
+                </div>
             </div>
-        </div>
+        </a>
         <button class="btn btn-outline-primary" data-is="none" ref="IisSubed" v-on:click="subsFunction"></button>
     </div>
 </template>
@@ -14,54 +16,59 @@
     import router from "@/router";
     import axios from "axios";
     import { defineProps, ref, onMounted } from "vue";
-    import { useStore } from 'vuex'
-    const store = useStore()
-    const props = defineProps(["id"])
-    let IuserIMG = ref(null)
-    let IairName = ref(null)
-    let IsubCount = ref(null)
-    let IisSubed = ref(null)
+    import { useStore } from "vuex";
+    const store = useStore();
+    const props = defineProps(["id"]);
+    let IuserIMG = ref(null);
+    let IairName = ref(null);
+    let IsubCount = ref(null);
+    let IisSubed = ref(null);
     const headers = {
-            "Content-Type": "application/json; charset=utf-8",
-            "Authorization": store.state.token,
-            "userid" : store.state.userid
-        }
+        "Content-Type": "application/json; charset=utf-8",
+        Authorization: store.state.token,
+        userid: store.state.userid,
+    };
     let body = {
-            aid : props.id,
-            userid : store.state.userid
+        aid: props.id,
+        userid: store.state.userid,
+    };
+
+    async function subsFunction() {
+        if (IisSubed.value.dataset.is != "none") {
+            await axios.post(store.state.axiosLink + "/api/article/read/subcard/add", body, { headers }).then(function (res) {
+                alert(res.data);
+                router.go(0);
+            });
+        } else {
+            router.push("./login");
+        }
     }
 
-    async function subsFunction(){
-        if(IisSubed.value.dataset.is!="none"){
-            await axios.post(store.state.axiosLink+"/api/article/read/subcard/add",body, {headers})
-            .then(function(res){
-                alert(res.data)
-                router.go(0)
+    async function getCardInfo() {
+        await axios
+            .post(store.state.axiosLink + "/article/read/subcard", body, { headers })
+            .then(function (res) {
+                IuserIMG.value.src = res.data.articleUserImg;
+                IairName.value.textContent = res.data.articleUserName;
+
+                //나중에 유효성검사할때 천, 만, 십만, 백만단위 생각
+                IsubCount.value.textContent = `구독자 ${res.data.subCount}명`;
+                if (res.data.isgudoked == "true") {
+                    IisSubed.value.dataset.is = "true";
+                    IisSubed.value.textContent = "구독 중";
+                } else {
+                    IisSubed.value.dataset.is = "false";
+                    IisSubed.value.textContent = "구독 하기";
+                }
             })
-        }else{
-            router.push("./login")
-        }
-        }
-
-    async function getCardInfo(){
-
-        await axios.post(store.state.axiosLink+"/article/read/subcard", body, { headers })
-        .then(function(res){
-            IuserIMG.value.src = res.data.articleUserImg
-            IairName.value.textContent = res.data.articleUserName
-        
-            //나중에 유효성검사할때 천, 만, 십만, 백만단위 생각
-            IsubCount.value.textContent = `구독자 ${res.data.subCount}명`
-            if(res.data.isgudoked=="true"){
-                IisSubed.value.dataset.is = "true"
-                IisSubed.value.textContent = "구독 중"
-            }else{
-                IisSubed.value.dataset.is = "false"
-                IisSubed.value.textContent = "구독 하기"
-            }
-        }).catch(e=>console.log(e))
+            .catch((e) => console.log(e));
     }
     onMounted(() => {
-        getCardInfo()
-    })
+        getCardInfo();
+    });
 </script>
+<style scoped lang="sass">
+    a
+        text-decoration: none
+        color: inherit
+</style>
