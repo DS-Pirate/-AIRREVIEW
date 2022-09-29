3d3e2b45674e1a93e20da7621f89f9f19efd3294
Hyunseok Byun <gumyoincirno@gmail.com>
2022-09-29 11:49:18 +0900

    Implement redirect to user page and diplay image for card

--- frontend/src/components/RecommentList.vue
+++ frontend/src/components/RecommentList.vue
@@ -1,10 +1,8 @@
 <template>
     <section class="recommend w-15">
         <span class="title">추천 게시글</span>
-
-        <div class="row-cols-1 py-4" v-for="(info, idx) in articleStat" v-bind:key="idx">
-            <Cards v-bind:cardInfo="info" v-bind:uInfo="userInfo[idx]" v-if="props.id!=info.aid"></Cards>
-            <span v-else><hr></span>
+        <div class="row-cols-1 py-4" v-for="(info, idx) in state.hashInfo" v-bind:key="idx">
+            <Cards v-bind:cardInfo="info.articles[info.articles.length-1]" v-if="props.id!=info.aid" :uImage="info.ImageName" :uInfo="info.userInfo[0]"></Cards>
         </div>
     </section>
 
@@ -13,34 +11,25 @@
     import axios from "axios";
     import { defineProps, reactive } from "vue";
     import Cards from "./ArticleRecommendCard.vue";
-    let articleStat = reactive([])
-    let artiInfo = reactive([])
-    let userInfo = reactive([])
+    let state = reactive({
+        hashInfo : []
+    })
     let props = defineProps(["id"]);
     let body = reactive({
         aid: props.id,
         reqPage: 0,
     });
 
-    function updateStates(aState, aInfo, uInfo) {
-        articleStat.push(...aState)
-        artiInfo.push(aInfo)
-        userInfo.push(...uInfo)
-    }
+
 
     function getCardInfoByHashTagWithAid() {
         axios
             .post("./article/articlerecommend/", body)
             .then(function (res) {
-                if (res.data.pageTotalCount == artiInfo.reqPage + 1) {
-                    updateStates(res.data.articles, -999, res.data.userInfo);
-                } else {
-                    updateStates(res.data.articles, res.data.articles.length, res.data.userInfo);
-                }
+                state.hashInfo.push(res.data)
             })
             .catch(function (error) {
-                console.log(error);
-                updateStates(null, -999);
+                console.log(error)
             });
     }
 
