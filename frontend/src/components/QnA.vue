44511781a3d4117b95275e70b50fd79e705481c3
Kim-DH0 <101510236+Kim-DH0@users.noreply.github.com>
2022-09-29 17:50:19 +0900

    [도현] QnA 프론트 작업

--- frontend/src/components/QnA.vue
+++ frontend/src/components/QnA.vue
@@ -1,63 +1,21 @@
 <template>
-<h1>Q&A</h1>
+<h1>Q&A</h1><br>
 <div class="accordion accordion-flush" id="accordionFlushExample">
-  <div class="accordion-item">
+  <div v-for="info, idx in state.questionlist" :key="(info, idx)" class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        <h5>나태일</h5> 
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#flush-collapseOne' + idx" aria-expanded="true" aria-controls="flush-collapseOne">
+        <h5>{{info.title}}</h5>
       </button>
     </h2>
-    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">이태일씨는 왜 <code>나태일</code> 인가요? 
-          <hr><div class="p-1"><p>나태일이기 때문입니다.</p></div>
-       </div>
-    </div>
-  </div>
-
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-
-        <h5>이태일씨의 야스쿠니 신사에 대해서</h5>
-      </button>
-    </h2>
-    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">이태일씨는 야스쿠니 신사에서 며칠에 한 번 참배하나요?
-        <hr><div class="p-1"><span>지금 순간에도 참배 중입니다.</span></div>
-      </div>
-
-    </div>
-  </div>
-
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-        <h5>도요토미 이태일시</h5>
-      </button>
-    </h2>
-    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">とよとみいているし
-      <hr><div class="p-1"><span>그대로 읽으시면 됩니다.</span></div>
-      </div>
-
-    </div>
-  </div>
-
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingFour">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
-        <h5>이토 태일부미의 폭탄</h5>
-      </button>
-    </h2>
-    <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">이토 태일부미가 안도현 의사에게 도시락 폭탄을 맞은 날짜는 언제인가요?
-        <hr><div class="p-1"><span>매일 맞고 있습니다.</span></div>
+    <div :id="'flush-collapseOne' + idx" class="accordion-collapse collapse px-4" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">{{info.context}}<hr>
+        <div><p>{{info.answerContext}}</p></div>
       </div>
     </div>
   </div>
 </div>
 </template>
-<!-- <script setup>
+<script setup>
 import { reactive } from 'vue';
 import axios from 'axios';
 import store from '@/store';
@@ -80,6 +38,6 @@ axios.post(url, store.state.userid, { headers }).then(function (res) {
 
 </script>
 <style scoped lang="sass">
-  h1
-    text-align: center
-</style> -->
+  .accordion-flush 
+    background-color:gray
+</style>
