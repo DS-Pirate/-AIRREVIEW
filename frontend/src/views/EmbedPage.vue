b51ffbd5b8f7db8b447386da7b6b714497084b67
Hyunseok Byun <gumyoincirno@gmail.com>
2022-10-07 23:01:21 +0900

    Update recommendsection to appearing when hovered

--- frontend/src/views/EmbedPage.vue
+++ frontend/src/views/EmbedPage.vue
@@ -7,7 +7,7 @@
                 </div>
                 <hr />
                 <div class="preview-context d-flex gap-2 align-items-top">
-                    <img :src="`${store.state.EmbedLink}/airreview/images/read/${embedInfo.img}`" alt="image" class="w-30 preview-context-thumbnailimg img-fluid" v-show="inputarea.thumbnail" />
+                    <img :src="`${store.state.EmbedLink}/images/read/${embedInfo.img}`" alt="image" class="w-30 preview-context-thumbnailimg img-fluid" v-show="inputarea.thumbnail" />
                     <div class="preview-context-context w-70 d-flex flex-column gap-3">
                         <span class="preview-context-context-rating" v-show="inputarea.rating">★★★★★</span>
                         <p class="preview-context-context_description" v-html="embedInfo.context"></p>
